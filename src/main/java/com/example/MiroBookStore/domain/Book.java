3d35cc91a73a07f54054aa5793828cee359a371c
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-02-02 19:15:44 +0200

    Initial commit

---
+++ src/main/java/com/example/MiroBookStore/domain/Book.java
@@ -0,0 +1,10 @@
+package com.example.MiroBookStore.domain;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int year;
+    private String isbn;
+    private double price;
+}
---
+++ src/test/java/com/example/MiroBookStore/MiroBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiroBookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiroBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
