06ed7268c435bc2da40b7c5763cdb0220f150a79
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-03-28 21:10:21 +0300

    Initial commit

--- src/main/java/com/example/MiroBookStore/MiroBookstoreApplication.java
+++ src/main/java/com/example/MiroBookStore/MiroBookstoreApplication.java
@@ -2,12 +2,17 @@ package com.example.MiroBookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class MiroBookstoreApplication {
+public class MiroBookstoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiroBookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
